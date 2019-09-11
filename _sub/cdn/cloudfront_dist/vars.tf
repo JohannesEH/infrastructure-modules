ba43bdb9a00fb3861c44a604abbaeffd22420ac3
Dieb Sami Maher (DFDS A/S) <samdi@DFDS.COM>
2019-09-11 15:55:35 +0200

    minor change for cf vars

--- _sub/cdn/cloudfront_dist/vars.tf
+++ _sub/cdn/cloudfront_dist/vars.tf
@@ -17,6 +17,12 @@ variable "acm_certificate_arn" {
   default = ""
 }
 
+variable "custom_ssl_security_policy" {
+  description = "Choose the security policy that you want CloudFront to use for HTTPS connections. A security policy determines two settings: the SSL/TLS protocol that CloudFront uses to communicate with viewers, and the cipher that CloudFront uses to encrypt the content that it returns to viewers. When you use a custom SSL certificate and SNI, you must use TLSv1 or later. We recommend that you specify TLSv1.1_2016 unless your users are using browsers or devices that do not support TLSv1.1 or later. When you use a custom SSL certificate and dedicated IP addresses, we recommend that you use TLSv1. In this configuration, the TLSv1_2016, TLSv1.1_2016 and TLSv1.2_2018 security policies are not available"
+  default = "TLSv1.2_2018"
+}
+
+
 variable "aliases" {
   default = []
   type = "list"
