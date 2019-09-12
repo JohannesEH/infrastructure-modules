53fd89cac6f1a53c1df75d3af90b9bd0a1d8cfdb
Dieb Sami Maher (DFDS A/S) <samdi@DFDS.COM>
2019-09-12 08:28:18 +0200

    minor fix lambda edge outputs

--- cdn/lambda-edge/outputs.tf
+++ cdn/lambda-edge/outputs.tf
@@ -3,5 +3,5 @@ output "lambda_function_arn" {
 }
 
 output "lambda_function_qualified_arn" {
-  value = "${module.lambda_function_qualified_arn}"
+  value = "${module.aws_lambda_edge_function.lambda_function_qualified_arn}"
 }
