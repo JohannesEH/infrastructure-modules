c8c6422f9836ea8651fa7c4fb8d944b7132a03ed
Dieb Sami Maher (DFDS A/S) <samdi@DFDS.COM>
2019-09-12 15:14:02 +0200

    s3 web site - no assume

--- static-content-hosting/s3-app/main.tf
+++ static-content-hosting/s3-app/main.tf
@@ -7,9 +7,9 @@ provider "aws" {
   region  = var.aws_region
   version = "~> 2.15"  # from 2.11 Minimum required 2.14
 
-  assume_role {
-    role_arn = var.aws_assume_role_arn
-  }
+  # assume_role {
+  #   role_arn = var.aws_assume_role_arn
+  # }
 }
 
 module "s3_app" { 
