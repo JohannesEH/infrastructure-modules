3f796d13280108f5f0953d4d878b5a0c06bbd8bd
Dieb Sami Maher (DFDS A/S) <samdi@DFDS.COM>
2019-09-12 08:24:48 +0200

    lambda edge outputs

--- cdn/lambda-edge/outputs.tf
+++ cdn/lambda-edge/outputs.tf
@@ -1,3 +1,7 @@
 output "lambda_function_arn" {
   value = "${module.aws_lambda_edge_function.lambda_function_arn}"
 }
+
+output "lambda_function_qualified_arn" {
+  value = "${module.lambda_function_qualified_arn}"
+}
