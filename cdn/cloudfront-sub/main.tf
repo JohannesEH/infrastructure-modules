c129c1cbc5c275357b4842df6dc0a7ce39973e55
Dieb Sami Maher (DFDS A/S) <samdi@DFDS.COM>
2019-09-12 07:49:25 +0200

    minor fix cf

--- cdn/cloudfront-sub/main.tf
+++ cdn/cloudfront-sub/main.tf
@@ -34,7 +34,7 @@ module "aws_cf_dist" {
   comment = var.cf_dist_comment
   aliases = "${length(var.cf_dist_domain_name) == 0 ? [] : [var.cf_dist_domain_name]}"
   # lambda_edge_qualified_arn = "${var.deploy_lambda_edge_func ? module.aws_cf_dist_default_behavior_lambda_function.lambda_function_qualified_arn : null }"
-   # FIXME: It's not enough to remove the association. You need to wait for at least 30 minutes before AWS removes all replicas of the lambda@edge function
+   # FIXME: It's not enough to remove the association. You need to wait for at least 30 minutes before AWS removes all replicas of the lambda@edge function. See: https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/lambda-edge-delete-replicas.html
    # TODO: A local exec function that listen to 
   origin_access_identity = "${module.aws_cf_oai.origin_access_identity}"
   logging_enable = "${var.cf_dist_logging_enable}"  
@@ -42,37 +42,3 @@ module "aws_cf_dist" {
   logging_bucket = "${var.cf_dist_logging_bucket}"
   logging_prefix = "${var.cf_dist_logging_prefix}"
 }
-
-# --------------------------------------------------
-# Default behavior lambda@edge setup
-# --------------------------------------------------
-
-# # Lambda@funtion to manipulating http request
-# module "aws_cf_dist_default_behavior_lambda_function" { 
-#   source = "../../_sub/compute/lambda-edge"
-#   deploy = "${var.deploy_lambda_edge_func}"
-#   lambda_function_name = "${local.lamda_edge_prefix}-cf-redirect-rules"
-#   lambda_role_name = "${local.lamda_edge_prefix}-cf-default-redirect-rules"
-
-#   lambda_function_handler = "${var.cf_dist_lambda_function_handler}" 
-#   # aws_region = "${var.aws_region}"
-#   s3_bucket = "${module.s3_bucket.bucket_name}"
-#   s3_key = "${module.s3_object_upload_lambda_edge.s3_object_key}"
-#   publish = true
-# }
-
-# # TODO: should be produced via CD pipeline
-# module "s3_object_upload_lambda_edge" { 
-#   source = "../../_sub/misc/s3-bucket-object"
-#   deploy = "${var.deploy_lambda_edge_func}"
-#   s3_bucket = "${module.s3_bucket.bucket_name}"
-#   key = "${var.cf_dist_lambda_edge_zip_filepath}"
-#   filepath = "${var.cf_dist_lambda_edge_zip_filepath}"
-# }
-
-# # Bucket for lambda function
-# module "s3_bucket" { 
-#   source = "../../_sub/storage/s3-bucket"
-#   deploy = "${var.deploy_lambda_edge_func ? 1: 0}" # TODO: Change to boolean (requires change in sub module)
-#   s3_bucket = "${var.cf_dist_lambda_s3bucket}"
-# }
