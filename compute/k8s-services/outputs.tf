bf3c42c8415811377532899967c0dabc16cee35c
Rune Abrahamsson <rune.abrahamsson@dfds.com>
2019-12-02 12:25:37 +0100

    uncommented outputs for traefik and argo

--- compute/k8s-services/outputs.tf
+++ compute/k8s-services/outputs.tf
@@ -6,19 +6,19 @@ output "traefik_alb_anon_dns_name" {
     value = "${element(concat(module.traefik_alb_anon_dns.record_name, list("")), 0)}.${var.workload_dns_zone_name}"
 }
 
-# output "traefik_alb_auth_dns_name" {
-#     value = "${element(concat(module.traefik_alb_auth_dns.record_name, list("")), 0)}.${var.workload_dns_zone_name}"
-# }
+output "traefik_alb_auth_dns_name" {
+    value = "${element(concat(module.traefik_alb_auth_dns.record_name, list("")), 0)}.${var.workload_dns_zone_name}"
+}
 
 
 # --------------------------------------------------
 # ArgoCD
 # --------------------------------------------------
 
-# output "argocd_endpoint" {
-#   value = "${module.argocd_deploy.external_url}"
-# }
+output "argocd_endpoint" {
+  value = "${module.argocd_deploy.external_url}"
+}
 
-# output "argocd_grpc_endpoint" {
-#   value = "${module.argocd_deploy.grpc_host_url}"
-# }
+output "argocd_grpc_endpoint" {
+  value = "${module.argocd_deploy.grpc_host_url}"
+}
